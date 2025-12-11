89a1555ff3489e6665b03d1023294dae462813d1
SmartSakshi13 <sakshibhosale0099@gmail.com>
2025-12-11 06:18:05 +0000

    Instance variable program

---
+++ instanceVar.py
@@ -0,0 +1,27 @@
+# Instance variable
+# These variables belong to each object
+
+class robot:
+	def __init__(self,speed):
+		speed = speed
+
+r = robot(10)
+print(r.speed)
+
+
+'''
+class madhun object banavto tevha tya object chya aat jua goshti sathavlya jatat , tyana instance variable mhantat .
+prteck object la independent variables aastat.
+
+class employee :
+	company = mycompant ========= class variable - there is sing copy of this class variable which is shared among emp1,emp2,emp3
+	emp_id
+	name 
+	basic salary ======== all there are instance variable 
+
+we can also say that ==== if the value of a variable varies from object to object , then such variable are called instance variable
+======================================================================================================================================================
+
+self vaprun specific tya object che variable aani functions access karto
+
+
