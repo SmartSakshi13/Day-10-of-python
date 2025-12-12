60cd79539c272fb7096b856aa53b3d5478b590ab
SmartSakshi13 <sakshibhosale0099@gmail.com>
2025-12-12 14:23:22 +0000

    Magic Dunder Methods program

---
+++ dunder.py
@@ -0,0 +1,16 @@
+'''
+__str__() --> print object
+__len__() 
+__add__() --> operator overloading
+'''
+
+class book:
+	def __init__(self,pages):
+		self.pages = pages
+
+	def __add__(self,other):
+		return self.pages + other.pages
+
+b1 = book(100)
+b2 = book(200)
+print(b1 + b2)
